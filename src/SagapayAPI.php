5b793ff7aeb2848825303f557d229c78de06f060
harensarma <harensarma@gmail.com>
2023-05-15 19:58:12 +0530

    fixed deposit issue

--- src/SagapayAPI.php
+++ src/SagapayAPI.php
@@ -5,64 +5,67 @@ namespace Sagapay;
 use Exception;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Support\Facades\Log;
 
 class SagapayAPI
 {
     private $apiKey;
     private $secretKey;
-    private $baseUrl = 'https://app.sagapay.net/api/v2';
+    private $baseUrl = 'https://app.sagapay.net';
     private $client;
 
     public function __construct($apiKey, $secretKey = null)
     {
-        $this->apiKey    = $apiKey;
+        $this->apiKey = $apiKey;
         $this->secretKey = $secretKey;
-        $this->client    = new Client([
-                                          'base_uri' => $this->baseUrl,
-                                          'headers'  => [
-                                              'Content-Type' => 'application/json',
-                                          ],
-                                      ]);
+        $this->client = new Client([
+            'base_uri' => $this->baseUrl,
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ],
+        ]);
     }
 
-    public function deposit($token_id, $amount, $ipn_url, $udf=null)
+    public function deposit($token_id, $amount, $ipn_url, $udf = null)
     {
+        echo 'working here<br/>';
         try {
-            $response = $this->client->post('/deposit', [
+            $response = $this->client->post('/api/v2/deposit', [
                 'json' => [
-                    'api_key'  => $this->apiKey,
+                    'api_key' => $this->apiKey,
                     'token_id' => $token_id,
-                    'ipn_url'  => $ipn_url,
-                    'amount'   => $amount,
-                    'udf'      => $udf,
+                    'ipn_url' => $ipn_url,
+                    'amount' => $amount,
+                    'udf' => $udf,
                 ],
             ]);
-
+var_dump($response);
+            echo 'working here too<br/>';
             return json_decode($response->getBody(), true);
-        }
-        catch (GuzzleException $e) {
+        } catch (Exception $e) {
+
+           var_dump($e->getMessage());
             return json_decode($e->getResponse()->getBody(), true);
         }
     }
 
-    public function withdraw($token_id, $amount, $withdrawal_address, $ipn_url, $udf=null)
+    public function withdraw($token_id, $amount, $withdrawal_address, $ipn_url, $udf = null)
     {
         try {
-            $response = $this->client->post('/withdraw', [
+            $response = $this->client->post('/api/v2/withdraw', [
                 'json' => [
-                    'api_key'            => $this->apiKey,
-                    'api_secret'         => $this->secretKey,
-                    'ipn_url'            => $ipn_url,
-                    'token_id'           => $token_id,
-                    'amount'             => $amount,
+                    'api_key' => $this->apiKey,
+                    'api_secret' => $this->secretKey,
+                    'ipn_url' => $ipn_url,
+                    'token_id' => $token_id,
+                    'amount' => $amount,
                     'withdrawal_address' => $withdrawal_address,
-                    'udf'                => $udf,
+                    'udf' => $udf,
                 ],
             ]);
 
             return json_decode($response->getBody(), true);
-        }
-        catch (GuzzleException $e) {
+        } catch (GuzzleException $e) {
             return json_decode($e->getResponse()->getBody(), true);
         }
     }
@@ -70,25 +73,24 @@ class SagapayAPI
     public function checkBalance($token_id)
     {
         try {
-            $response = $this->client->post('/check-balance', [
+            $response = $this->client->post('/api/v2/check-balance', [
                 'json' => [
-                    'api_key'  => $this->apiKey,
+                    'api_key' => $this->apiKey,
                     'token_id' => $token_id,
                 ],
             ]);
 
             return json_decode($response->getBody(), true);
-        }
-        catch (GuzzleException $e) {
+        } catch (GuzzleException $e) {
             return json_decode($e->getResponse()->getBody(), true);
         }
     }
 
     public function validateIPN($request, $secretKey)
     {
-        $payload           = json_encode($request->getContent());
+        $payload = json_encode($request->getContent());
         $receivedSignature = $request->headers->get('x-sagapay-ipn');
-        $hmac              = hash_hmac('sha256', $payload, $secretKey);
+        $hmac = hash_hmac('sha256', $payload, $secretKey);
 
         return $hmac === $receivedSignature;
     }
